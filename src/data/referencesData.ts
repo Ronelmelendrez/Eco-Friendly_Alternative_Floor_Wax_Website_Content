434f60ea746b83cd1716ede29512da1d0cf79331
Ronel <ronelmelendrez01@gmail.com>
2026-05-12 23:03:57 +0800

    feat: add data reference data

---
+++ src/data/referencesData.ts
@@ -0,0 +1,7 @@
+export const references = [
+  { authors: "Alcaraz et al.", year: 2019, title: "Effectiveness of Banana Peel as Alternative Floor Wax", type: "Journal" },
+  { authors: "Mcleod", year: 2023, title: "P-Value and Statistical Significance", type: "Article" },
+  { authors: "Philippine Statistics Authority", year: 2022, title: "Coconut and Banana Production Statistics", type: "Report" },
+  { authors: "Better Homes & Gardens", year: 2022, title: "Natural Cleaning Solutions", type: "Magazine" },
+  { authors: "Industrial Crops and Products Journal", year: 2004, title: "Lauric Acid Applications", type: "Journal" }
+]
